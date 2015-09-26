a7cb8e41fce67f22ec9e95e65aa5bb3bde9e5d6e
Kelly Munro <kellymdev@gmail.com>
2015-09-26 16:33:25 +1200

    Add is_computer flag to player

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925091731) do
+ActiveRecord::Schema.define(version: 20150926042844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,10 +45,11 @@ ActiveRecord::Schema.define(version: 20150925091731) do
 
   create_table "players", force: :cascade do |t|
     t.string   "name"
-    t.integer  "score",      default: 0
+    t.integer  "score",       default: 0
     t.integer  "game_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "is_computer", default: false
   end
 
 end
