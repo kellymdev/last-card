7a5f8c8200350f7f90c12de036ea2c357655f286
Kelly Munro <kellymdev@gmail.com>
2015-09-26 16:42:10 +1200

    Create a computer player when the game is created

--- spec/services/create_game_spec.rb
+++ spec/services/create_game_spec.rb
@@ -14,12 +14,22 @@ RSpec.describe CreateGame, type: :service do
 
     context "game creation" do
       it { is_expected.to be_valid }
+
+      it "increases the number of games by one" do
+        expect { CreateGame.new.call }.to change { Game.count }.by 1
+      end
     end
 
     context "deck creation" do
       subject(:deck) { game.deck_cards.count }
 
       it { is_expected.to eq Game::DEFAULT_DECK_SIZE}
+    end
+
+    context "computer player creation" do
+      it "increases the number of players by one" do
+        expect { CreateGame.new.call }.to change { Player.count }.by 1
+      end
     end
   end
 end
