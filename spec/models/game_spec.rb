95731e752165877040e2d06fd2c55174048cd905
Kelly Munro <kellymdev@gmail.com>
2015-09-30 20:29:03 +1300

    Display the card to be played from the deck

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:card) { Card.create!(value: 3, suit: "hearts", points: 3) }
+  let!(:card2) { Card.create!(value: 9, suit: "clubs", points: 9) }
+  let(:game) { CreateGame.new.call }
+
+  describe "#next_card" do
+    subject { game.next_card }
+
+    it { is_expected.to eq card.deck_cards.first }
+  end
 end
